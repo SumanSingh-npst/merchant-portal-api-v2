075deab86b643f4cd11188605f88997ed08d6a3e
chandan naresh <chandan.sutradhar@npstx.com>
2024-08-14 13:52:20 +0530

    removed unused endpoints

--- src/recon/recon.controller.ts
+++ src/recon/recon.controller.ts
@@ -12,46 +12,4 @@ export class ReconController {
     return await this.reconService.initiate2WayRecon();
   }
 
-  @Get('/getTotalTXNCountByDate/:date')
-  async getTotalTXNCountByDate(@Param('date') date: string) {
-    console.log(date);
-    return await this.reconService.getTotalTXNCountByDate(date);
-  }
-
-
-  @Get('/getNPCICountByDate/:date')
-  async getNPCICountByDate(@Param('date') date: string) {
-    return await this.reconService.getNPCICountByDate(date);
-  }
-
-  @Get('/getTotalReconCountByDate/:date')
-  async getTotalReconCountByDate(@Param('date') date: string) {
-    return await this.reconService.getTotalReconCountByDate(date);
-  }
-
-  @Get('/getReconTXNS/:date')
-  async getReconTXNS(@Param('date') date: string) {
-    return await this.reconService.getReconTXNS(date);
-  }
-
-  @Get('/getSuccessTXNS/:date')
-  async getSuccessTXNS(@Param('date') date: string) {
-    return await this.reconService.getSuccessTXNS(date);
-  }
-
-  @Get('/getFailedTXNS/:date')
-  async getFailedTXNS(@Param('date') date: string) {
-    return await this.reconService.getFailedTXNS(date);
-  }
-
-  @Get('/getSuccessCount/:date')
-  async getSuccessCount(@Param('date') date: string) {
-    return await this.reconService.getSuccessCount(date);
-  }
-
-  @Get('/getFailureCount/:date')
-  async getFailureCount(@Param('date') date: string) {
-    return await this.reconService.getFailureCount(date);
-  }
-
 }
