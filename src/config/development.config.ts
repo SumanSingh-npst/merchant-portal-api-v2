c5b0cfcf75e0b88213fce3bab90b1575ac515f2d
chandan naresh <chandan.sutradhar@npstx.com>
2024-08-26 09:51:04 +0530

    dev and prod config added

--- src/config/development.config.ts
+++ src/config/development.config.ts
@@ -5,5 +5,5 @@ export default {
         username: 'default',
         password: 'CsRCIAHoU.3jJ',
     },
-    JWT_SECRET: '2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b',
+    JWT_SECRET: 'Teng!n@404',
 };
