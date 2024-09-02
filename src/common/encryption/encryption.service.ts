012661710dc806c815dc2f3eef15e691d31df15a
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-02 15:15:23 +0530

    "Refactored UserService: updated constructor, create user method, findOneByEmail method, and added encryption/decryption for OTPs"

--- src/common/encryption/encryption.service.ts
+++ src/common/encryption/encryption.service.ts
@@ -5,41 +5,48 @@ import * as crypto from 'crypto';
 
 @Injectable()
 export class EncryptionService {
+  constructor(private configSvc: ConfigService) {}
+  private readonly ENCRYPTION_KEY =
+    this.configSvc.get<string>('ENCRYPTION_KEY') ||
+    crypto.randomBytes(32).toString('hex'); // Should be 32 bytes for AES-256
+  private readonly IV_LENGTH = 16; // For AES, this is always 16
 
-    constructor(private configSvc: ConfigService) {
+  // Hashing with bcrypt (for passwords)
+  async hashData(data: string): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(data, salt);
+  }
 
-    }
-    private readonly ENCRYPTION_KEY = this.configSvc.get<string>("ENCRYPTION_KEY") || crypto.randomBytes(32).toString('hex'); // Should be 32 bytes for AES-256
-    private readonly IV_LENGTH = 16; // For AES, this is always 16
+  // Compare hashed data with a plain text (for passwords)
+  async compareHash(data: string, hashed: string): Promise<boolean> {
+    return bcrypt.compare(data, hashed);
+  }
 
-    // Hashing with bcrypt (for passwords)
-    async hashData(data: string): Promise<string> {
-        const salt = await bcrypt.genSalt();
-        return bcrypt.hash(data, salt);
-    }
+  // Encrypt data
+  encrypt(text: string): string {
+    const iv = crypto.randomBytes(this.IV_LENGTH);
+    const cipher = crypto.createCipheriv(
+      'aes-256-cbc',
+      Buffer.from(this.ENCRYPTION_KEY, 'hex'),
+      iv,
+    );
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+  }
 
-    // Compare hashed data with a plain text (for passwords)
-    async compareHash(data: string, hashed: string): Promise<boolean> {
-        return bcrypt.compare(data, hashed);
-    }
-
-    // Encrypt data
-    encrypt(text: string): string {
-        const iv = crypto.randomBytes(this.IV_LENGTH);
-        const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(this.ENCRYPTION_KEY, 'hex'), iv);
-        let encrypted = cipher.update(text);
-        encrypted = Buffer.concat([encrypted, cipher.final()]);
-        return iv.toString('hex') + ':' + encrypted.toString('hex');
-    }
-
-    // Decrypt data
-    decrypt(text: string): string {
-        const textParts = text.split(':');
-        const iv = Buffer.from(textParts.shift()!, 'hex');
-        const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-        const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(this.ENCRYPTION_KEY, 'hex'), iv);
-        let decrypted = decipher.update(encryptedText);
-        decrypted = Buffer.concat([decrypted, decipher.final()]);
-        return decrypted.toString();
-    }
+  // Decrypt data
+  decrypt(text: string): string {
+    const textParts = text.split(':');
+    const iv = Buffer.from(textParts.shift()!, 'hex');
+    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    const decipher = crypto.createDecipheriv(
+      'aes-256-cbc',
+      Buffer.from(this.ENCRYPTION_KEY, 'hex'),
+      iv,
+    );
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,201 +11,226 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectClickHouse() private readonly clickdb: ClickHouseClient,
+    private http: HttpService,
+    private logger: Logger,
+    private auditSvc: AuditService,
+    private encSvc: EncryptionService,
+    private configSvc: ConfigService,
+  ) {}
 
+  async create(user: User) {
+    //check if the user doesn't exists
+    const userExists = await this.findOneByEmail(user.email);
+    console.log(userExists);
+    if (userExists) {
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    }
 
-    constructor(
-        @InjectClickHouse() private readonly clickdb: ClickHouseClient,
-        private http: HttpService,
-        private logger: Logger,
-        private auditSvc: AuditService,
-        private encSvc: EncryptionService,
-        private configSvc: ConfigService
-    ) { }
+    const lastUser = await this.lastUser();
+    const user_id = lastUser.createdOn + 1;
 
-    async create(user: User) {
-
-        //check if the user doesn't exists
-        const userExists = await this.findOneByEmail(user.email);
-        console.log(userExists);
-        if (userExists) {
-            throw new HttpException('User already exists', HttpStatus.CONFLICT);
-        }
-
-        const user_id = Math.random().toString(36).substring(7);
-        //check if the id doesn't exists if yes then regenerate one.
-
-        user.blocked = false;
-        user.failedAttempt = 0;
-        user.createdOn = new Date().toISOString();
-        user.disabled = false;
-        user.passwordResetDate = new Date(new Date().setMonth(3)).toISOString();
-        user.lastLoggedIn = new Date().toISOString();
-        const query = `INSERT INTO USER (USER_ID, FULL_NAME, EMAIL, MOBILE, PASSWORD, 
+    user.failedAttempt = 0;
+    user.createdOn = new Date().toISOString();
+    user.disabled = false;
+    user.passwordResetDate = new Date(new Date().setMonth(3)).toISOString();
+    user.lastLoggedIn = new Date().toISOString();
+    const query = `INSERT INTO USER (USER_ID, FULL_NAME, EMAIL, MOBILE, PASSWORD, 
         LAST_LOGGED_IN,BLOCKED, FAILED_ATTEMPT, CREATED_ON, DISABLED, PASSWORD_RESET_DATE) 
         VALUES ('${user_id}','${user.fullName}','${user.email}','${user.mobile}','${user.password}',
         '${user.lastLoggedIn}','${user.blocked}','${user.failedAttempt}','${user.createdOn}','${user.disabled}', '${user.passwordResetDate}');`;
 
-        //roles will be multiple values in array
-
-        const rolesQuery = `INSERT INTO USER_ROLES (USER_ID, ROLE_ID) VALUES`;
-        const values = user.roles.map(role => `('${user_id}','${role}')`).join(',');
-        const finalRolesQuery = `${rolesQuery} ${values};`;
-        try {
-            const r = await this.clickdb.exec({ query: query });
-            const r2 = await this.clickdb.exec({ query: finalRolesQuery });
-            return user;
-        } catch (error) {
-            throw error;
-        }
+    const rolesQuery = `INSERT INTO USER_ROLES (USER_ID, ROLE_ID) VALUES`;
+    const values = user.roles
+      .map((role) => `('${user_id}','${role}')`)
+      .join(',');
+    const finalRolesQuery = `${rolesQuery} ${values};`;
+    try {
+      await this.clickdb.exec({ query: query });
+      await this.clickdb.exec({ query: finalRolesQuery });
+      return user;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async findOneByEmail(email: string) {
-        const query = `SELECT * FROM USER WHERE EMAIL = '${email}';`;
-        try {
-            const r = await this.clickdb.query({ query: query });
-            const jsonRes: any = await r.json();
-            if (jsonRes.data.length === 0) {
-                return null;
-            }
-            const user: User = {
-                userId: jsonRes.data[0].USER_ID,
-                fullName: jsonRes.data[0].FULL_NAME,
-                password: jsonRes.data[0].PASSWORD,
-                lastLoggedIn: jsonRes.data[0].LAST_LOGGED_IN,
-                email: jsonRes.data[0].EMAIL,
-                mobile: jsonRes.data[0].MOBILE,
-                blocked: jsonRes.data[0].BLOCKED,
-                failedAttempt: jsonRes.data[0].FAILED_ATTEMPT,
-                createdOn: jsonRes.data[0].CREATED_ON,
-                disabled: jsonRes.data[0].DISABLED,
-                passwordResetDate: jsonRes.data[0].PASSWORD_RESET_DATE,
-                roles: []
-            };
+  async findOneByEmail(email: string) {
+    const query = `SELECT * FROM USER WHERE EMAIL = '${email}';`;
+    try {
+      const r = await this.clickdb.query({ query: query });
+      const jsonRes: any = await r.json();
+      if (jsonRes.data.length === 0) {
+        return null;
+      }
+      const user: User = {
+        userId: jsonRes.data[0].USER_ID,
+        fullName: jsonRes.data[0].FULL_NAME,
+        password: jsonRes.data[0].PASSWORD,
+        lastLoggedIn: jsonRes.data[0].LAST_LOGGED_IN,
+        email: jsonRes.data[0].EMAIL,
+        mobile: jsonRes.data[0].MOBILE,
+        blocked: jsonRes.data[0].BLOCKED,
+        failedAttempt: jsonRes.data[0].FAILED_ATTEMPT,
+        createdOn: jsonRes.data[0].CREATED_ON,
+        disabled: jsonRes.data[0].DISABLED,
+        passwordResetDate: jsonRes.data[0].PASSWORD_RESET_DATE,
+        roles: [],
+      };
 
-            const rolesQuery = `SELECT ROLE_ID FROM USER_ROLES WHERE USER_ID = '${user.userId}';`;
-            const r2 = await this.clickdb.query({ query: rolesQuery });
+      const rolesQuery = `SELECT ROLE_ID FROM USER_ROLES WHERE USER_ID = '${user.userId}';`;
+      const r2 = await this.clickdb.query({ query: rolesQuery });
 
-            const jsonRes2: any = await r2.json();
+      const jsonRes2: any = await r2.json();
 
-            const rolePromises = await jsonRes2.data.map(async role => {
-                const r = await this.clickdb.query({ query: `SELECT ROLE_NAME FROM ROLE WHERE ROLE_ID = '${role.ROLE_ID}';` });
-                const res = await r.json();
-                return res.data[0];
-            });
+      const rolePromises = await jsonRes2.data.map(async (role) => {
+        const r = await this.clickdb.query({
+          query: `SELECT ROLE_NAME FROM ROLE WHERE ROLE_ID = '${role.ROLE_ID}';`,
+        });
+        const res = await r.json();
+        return res.data[0];
+      });
 
-            const roles = await Promise.all(rolePromises);
-            user.roles = roles.map(role => role.ROLE_NAME);
-            return user;
+      const roles = await Promise.all(rolePromises);
+      user.roles = roles.map((role) => role.ROLE_NAME);
+      return user;
 
-            // for (const role of jsonRes2.data) {
-            //     //fetch role name from role id
+      // for (const role of jsonRes2.data) {
+      //     //fetch role name from role id
 
-            //     user.roles.push(role.ROLE_ID);
-            // }
-            return user;
-        } catch (error) {
-            throw error;
-        }
-
+      //     user.roles.push(role.ROLE_ID);
+      // }
+      return user;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async saveOTP(userId: string, otp: string, otpType: string, request_id: string) {
-
-        const insert = `INSERT INTO OTP_VERIFICATION (USER_ID,OTP_TYPE,OTP_VALUE,CREATED_ON,REQUEST_ID) 
+  async saveOTP(
+    userId: string,
+    otp: string,
+    otpType: string,
+    request_id: string,
+  ) {
+    const insert = `INSERT INTO OTP_VERIFICATION (USER_ID,OTP_TYPE,OTP_VALUE,CREATED_ON,REQUEST_ID) 
         VALUES  ('${userId}','${otpType}', '${otp}','${new Date().toISOString()}', '${request_id}');`;
-        try {
-            const r = await this.clickdb.exec({ query: insert });
-            return true;
-        } catch (error) {
-            throw error;
-        }
-
+    try {
+      await this.clickdb.exec({ query: insert });
+      return true;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async sendOTP(email: string, fullName: string, userId: string, otpType: string) {
-        const otp = Math.floor(100000 + Math.random() * 900000);
-        //save the otp against the user first 
+  async sendOTP(
+    email: string,
+    fullName: string,
+    userId: string,
+    otpType: string,
+  ) {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    //save the otp against the user first
 
-        const payload = {
-            "from": {
-                "address": "info@timepayonline.com",
-                "name": "evok timepay"
-            },
-            "to": [
-                {
-                    "email_address": {
-                        "address": email,
-                        "name": fullName,
-                    }
-                }
-            ],
-            "subject": "Email Verification",
-            "htmlbody": "<div><b> Your OTP for Email Verification is " + otp + ".  </b></div>"
-        }
+    const payload = {
+      from: {
+        address: 'info@timepayonline.com',
+        name: 'evok timepay',
+      },
+      to: [
+        {
+          email_address: {
+            address: email,
+            name: fullName,
+          },
+        },
+      ],
+      subject: 'Email Verification',
+      htmlbody:
+        '<div><b> Your OTP for Email Verification is ' + otp + '.  </b></div>',
+    };
 
-        const headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': this.configSvc.get('zohoKey')
-        }
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: this.configSvc.get('zohoKey'),
+    };
 
-        const { data } = await firstValueFrom(
-            this.http.post('https://api.zeptomail.in/v1.1/email', payload, { headers }).pipe(
-                catchError((error: AxiosError) => {
-                    this.logger.error(error.response.data);
-                    throw 'An error happened!';
-                }),
-            ),
+    const { data } = await firstValueFrom(
+      this.http
+        .post('https://api.zeptomail.in/v1.1/email', payload, { headers })
+        .pipe(
+          catchError((error: AxiosError) => {
+            this.logger.error(error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    if (data.data && data.data[0].code == 'EM_104') {
+      const encOTP = await this.encSvc.encrypt(otp.toString());
+      await this.saveOTP(userId, encOTP, otpType, data.request_id);
+    } else {
+      throw 'An error encountered while sending otp!';
+    }
+    return data;
+  }
+
+  async verifyOTP(userId: string, otp: string, otpType: string) {
+    const query = `SELECT OTP_VALUE FROM OTP_VERIFICATION WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}';`;
+    try {
+      const r = await this.clickdb.query({ query: query });
+      const jsonRes: any = await r.json();
+      if (jsonRes.data.length === 0) {
+        throw new HttpException('Invalid OTP', HttpStatus.BAD_REQUEST);
+      } else {
+        //decrypt the otp
+        const decryptedOTP = await this.encSvc.decrypt(
+          jsonRes.data[0].OTP_VALUE,
         );
-
-        if (data.data && data.data[0].code == 'EM_104') {
-            const encOTP = await this.encSvc.encrypt(otp.toString());
-            await this.saveOTP(userId, encOTP, otpType, data.request_id);
+        console.log('otp compare=>', decryptedOTP, otp);
+        if (decryptedOTP === otp) {
+          await this.clickdb.exec({
+            query: `ALTER TABLE OTP_VERIFICATION UPDATE VERIFIED = true, VERIFIED_ON = '${new Date().toUTCString()}' 
+                    WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}';`,
+          });
+          return true;
         } else {
-            throw 'An error encountered while sending otp!';
+          throw new HttpException(
+            'FAILED TO UPDATE VERIFICATION STATUS',
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
         }
-        return data;
+      }
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async verifyOTP(userId: string, otp: string, otpType: string) {
-        const query = `SELECT OTP_VALUE FROM OTP_VERIFICATION WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}';`;
-        try {
-            const r = await this.clickdb.query({ query: query });
-            const jsonRes: any = await r.json();
-            if (jsonRes.data.length === 0) {
-                throw new HttpException('Invalid OTP', HttpStatus.BAD_REQUEST);
-            }
-            else {
-                //decrypt the otp
-                const decryptedOTP = await this.encSvc.decrypt(jsonRes.data[0].OTP_VALUE);
-                console.log('otp compare=>', decryptedOTP, otp);
-                if (decryptedOTP === otp) {
-                    await this.clickdb.exec({
-                        query: `ALTER TABLE OTP_VERIFICATION UPDATE VERIFIED = true, VERIFIED_ON = '${new Date().toUTCString()}' 
-                    WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}';`
-                    });
-                    return true;
-                }
-                else {
-                    throw new HttpException('FAILED TO UPDATE VERIFICATION STATUS', HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            }
-
-        } catch (error) {
-            throw error;
-        }
+  async isOTPVerified(userId: string, otpType: string) {
+    const query = `SELECT * FROM OTP_VERIFICATION WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}' AND VERIFIED = true;`;
+    try {
+      const r = await this.clickdb.query({ query: query });
+      const jsonRes: any = await r.json();
+      return jsonRes.data.length > 0 ? true : false;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async isOTPVerified(userId: string, otpType: string) {
-        const query = `SELECT * FROM OTP_VERIFICATION WHERE USER_ID = '${userId}' AND OTP_TYPE = '${otpType}' AND VERIFIED = true;`;
-        try {
-            const r = await this.clickdb.query({ query: query });
-            const jsonRes: any = await r.json();
-            return jsonRes.data.length > 0 ? true : false;
-        }
-        catch (error) {
-            throw error;
-        }
+  async lastUser() {
+    try {
+      const query = `SELECT FULL_NAME, CREATED_ON
+                    FROM "USER"
+                    ORDER BY CREATED_ON DESC
+                    LIMIT 1`;
+      const response = await this.clickdb.query({ query: query });
+      const data: any = await response.json();
+      return {
+        name: data.data[0].FULL_NAME,
+        createdOn: data.data[0].CREATED_ON,
+      };
+    } catch (error) {
+      return { res: error, status: false, msg: 'error', statusCode: 500 };
     }
+  }
 }
-
