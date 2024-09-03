a5f568273cc35541d78d1651a2ce57c30e06b684
xoaeb <shoaib.salim@npstx.com>
2024-09-03 11:43:49 +0530

    "Updated AuthModule and added comments to UserController"

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -40,3 +40,18 @@ export class UserController {
     return await this.otp.isOTPVerified(body.userId, body.otpType);
   }
 }
+ //1. //find by email
+//2. //create user => create user taking userId, email , password, failedAttempt
+  // createdOn
+  // disabled
+  // passwordResetDate
+  // lastLoggedIn
+//3. sendotp: insert new row in OTP_VERIFICATION table TAKING all defualt values mentioned in otpSvc
+//4. verifyOTP: update OTP_VERIFICATION table with verified otp provided by user
+//5. updateUser: update USER with new provided mobileno
+//6. sendotp: insert new row in OTP_VERIFICATION table TAKING all defualt values mentioned in otpSvc
+//7. emailVerStatus: provide userId to check verification field in OTP_VERIFICATION
+
+//...
+//8. PAN VER
+
