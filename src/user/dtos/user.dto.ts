653006af975a0463c44115217c517b228a72f06f
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-05 10:51:20 +0530

    "Updated User DTO class by adding static keyword to properties and reformatted SQL comment"

--- src/user/dtos/user.dto.ts
+++ src/user/dtos/user.dto.ts
@@ -1,19 +1,20 @@
 export class User {
-  userId?: string;
-  fullName?: string;
-  email: string;
-  password: string;
-  mobile: string;
-  lastLoggedIn?: string;
-  blocked?: boolean;
-  failedAttempt?: number;
-  createdOn?: string;
-  disabled?: boolean;
-  passwordResetDate?: string;
-  roles: string[];
+  static readonly userId?: string;
+  static readonly fullName?: string;
+  static readonly email: string;
+  static readonly password: string;
+  static readonly mobile: string;
+  static readonly lastLoggedIn?: string;
+  static readonly blocked?: boolean;
+  static readonly failedAttempt?: number;
+  static readonly createdOn?: string;
+  static readonly disabled?: boolean;
+  static readonly passwordResetDate?: string;
+  static readonly roles: string[];
 }
 
-// CREATE TABLE USER (USER_ID String, FULL_NAME String, EMAIL String, MOBILE String, PASSWORD String,
-// LAST_LOGGED_IN String, BLOCKED String, FAILED_ATTEMPT String, CREATED_ON DateTime, DISABLED String, PASSWORD_RESET_DATE DateTime)
-// ENGINE = ReplacingMergeTree()
-// ORDER BY USER_ID
+// ? schema for the user table
+// * CREATE TABLE USER (USER_ID String, FULL_NAME String, EMAIL String, MOBILE String, PASSWORD String,
+// * LAST_LOGGED_IN String, BLOCKED String, FAILED_ATTEMPT String, CREATED_ON DateTime, DISABLED String, PASSWORD_RESET_DATE DateTime)
+// * ENGINE = ReplacingMergeTree()
+// * ORDER BY USER_ID
