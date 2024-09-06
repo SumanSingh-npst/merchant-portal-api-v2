6227ca8e59cad172b4d2c4c6d70188aaeb4ba5fe
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-06 15:33:12 +0530

    "Refactor update method to simplify response and add error handling; add new allTables method"

--- src/shared-resource/shared-resource.service.ts
+++ src/shared-resource/shared-resource.service.ts
@@ -12,21 +12,24 @@ export class SharedResourceService {
       const query = `ALTER TABLE "${body.tableName}" 
                     UPDATE ${body.property} = '${body.value}' 
                     WHERE ${body.identifier} = '${body.identifierValue}'`;
-      const response = await this.db.query({ query: query });
-      const data: any = await response.json();
-      return data.data.length > 0
-        ? {
-            data: 'successfully update your user!',
-            status: true,
-            msg: 'success',
-            statusCode: 200,
-          }
-        : {
-            data: null,
-            status: false,
-            msg: 'notfound',
-            statusCode: 400,
-          };
+      await this.db.query({ query: query });
+
+      return {
+        data: 'successfully update your user!',
+        status: true,
+        msg: 'success',
+        statusCode: 200,
+      };
+    } catch (error) {
+      console.log(error);
+      return { res: error, status: false, msg: 'error', statusCode: 500 };
+    }
+  }
+
+  private async allTables() {
+    try {
+      const query = `SHOW TABLES`;
+      await this.db.query({ query: query });
     } catch (error) {
       return { res: error, status: false, msg: 'error', statusCode: 500 };
     }
