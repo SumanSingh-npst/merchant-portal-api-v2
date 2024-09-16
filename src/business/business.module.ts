89f4b7b081776fda6fbfc9c4c8ea011f1feffb07
xoaeb <shoaib.salim@npstx.com>
2024-09-16 16:58:33 +0530

    adds sharedsvc

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BusinessService } from './business.service';
 import { BusinessController } from './business.controller';
 import { BankService } from './bank/bank.service';
+import { SharedResourceService } from 'src/shared-resource/shared-resource.service';
 
 @Module({
   controllers: [BusinessController],
-  providers: [BusinessService, BankService],
+  providers: [BusinessService, BankService, SharedResourceService],
 })
 export class BusinessModule {}
